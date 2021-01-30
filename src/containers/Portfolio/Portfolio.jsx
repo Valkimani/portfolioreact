1ba75446bd9ddc97711d5e35018fc3c5579972f9
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-29 20:13:36 -0500

    Added a Contacts, Links and Portfolio folders and created jsx files for each of the folders.

---
+++ src/containers/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>This is the protfolio</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
