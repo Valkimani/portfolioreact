520665d1c39e87c667f0d03d91a821d8bc4b6f8f
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-30 21:11:08 -0500

    Added a nav bar and imported link.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="./index.html">Valentine Kimani</a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+import {Link} from 'react-router-dom';
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link to="/"
+      className="navbar-brand">Valentine Kimani
+    </Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
     </button>
-    <aside className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-         <li className="nav-item">
-          <a className="nav-link" href="index.html">About</a>
-        </li>
-         <li className="nav-item">
-          <a className="nav-link" href="portfolio.html">Portfolio</a>
+    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/"
+            className="nav-link">About
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="links.html">Links</a>
+          <Link to="/about" className="nav-link">Portfolio
+          </Link>
         </li>
-        <li className="nav-item active">
-          <a className="nav-link" href="contact.html">Contacts</a>
+        <li className="nav-item">
+          <Link to="/contact"
+            className="nav-link">Links
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact"
+            className="nav-link">Contacts
+          </Link>
         </li>
       </ul>
-      </aside>
+    </div>
   </nav>
-    );
-};
+);
 
 export default Navbar;
